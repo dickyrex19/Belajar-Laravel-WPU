6f43d667f91a176f501762ebf9a01d5afdb25b21
dickyrex19 <enjoybeat19@gmail.com>
2021-08-07 12:18:12 +0700

    video ke 13 - Searching & Pagination

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,6 +1,23 @@
 @extends('layouts.main')
 @section('container')
-  <h2 class="mb-4">{{ $title }}</h2>
+  <h2 class="mb-3 text-center">{{ $title }}</h2>
+
+  <div class="row justify-content-center mb-3">
+    <div class="col-md-6">
+      <form action="/posts">
+        @if (request('category'))
+          <input type="hidden" name="category" value="{{ request('category') }}">
+        @endif
+        @if (request('author'))
+          <input type="hidden" name="author" value="{{ request('author') }}">
+        @endif
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Search Post.." name="search" value="{{ request('search') }}">
+          <button class="btn btn-danger" type="submit">Search</button>
+        </div>
+      </form>
+    </div>
+  </div>
   
   @if ($posts->count() > 0)
     <div class="card mb-3">
@@ -9,24 +26,20 @@
         <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
         <p>
           <small class="text-muted">
-            By <a href="/posts/author/{{ $posts[0]->author->username }}" class="card-text text-decoration-none">{{ $posts[0]->author->name }}</a> in <a href="/categories/{{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
+            By <a href="/posts?author={{ $posts[0]->author->username }}" class="card-text text-decoration-none">{{ $posts[0]->author->name }}</a> in <a href="/posts?category={{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
           </small>
         </p>
         <p class="card-text">{{ $posts[0]->excerpt }}</p>
         <a href="/posts/{{ $posts[0]->slug }}" class="btn btn-primary">Read More</a>
       </div>
     </div>
-    
-  @else
-   <h3 style="position: absolute; transform: translate(-50%, -50%); top: 50%; left:50%; color: red">Belum ada Post</h3>
-  @endif
 
   <div class="container">
     <div class="row">
       @foreach ($posts->skip(1) as $post)
         <div class="col-md-4 mb-3">
           <div class="card">
-            <div class="position-absolute px-3 py-2" style="background-color: rgba(0, 0, 0, 0.8)"><a href="/categories/{{ $post->category->slug }}" class="text-decoration-none text-white">{{ $post->category->name }}</a></div>
+            <div class="position-absolute px-3 py-2" style="background-color: rgba(0, 0, 0, 0.8)"><a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none text-white">{{ $post->category->name }}</a></div>
             <img src="https://source.unsplash.com/600x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">
             <div class="card-body">
               <h5 class="card-title">
@@ -34,7 +47,7 @@
               </h5>
               <p>
                 <small class="text-muted">
-                  By <a href="/posts/author/{{ $post->author->username }}" class="card-text text-decoration-none">{{ $post->author->name }}</a> {{ $post->created_at->diffForHumans() }}
+                  By <a href="/posts?author={{ $post->author->username }}" class="card-text text-decoration-none">{{ $post->author->name }}</a> {{ $post->created_at->diffForHumans() }}
                 </small>
               </p>
               <p class="card-text">{{ $post->excerpt }}</p>
@@ -44,6 +57,15 @@
         </div>
       @endforeach
     </div>
+  </div>
+
+  @else
+   <h3 style="position: absolute; transform: translate(-50%, -50%); top: 50%; left:50%; color: red">No Post Found</h3>
+  @endif
+
+  {{-- Using Pagination --}}
+  <div class="d-flex justify-content-end">
+    {{ $posts->links() }}
   </div>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -33,25 +33,11 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', [PostController::class, 'index']);
-Route::get('posts/{post:slug}', [PostController::class, 'show']);
+Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('/categories', function () {
     return view('categories', [
         'title' => 'Post Categories',
         'categories' => Category::all()
-    ]);
-});
-
-Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts', [
-        'title' => "Post by Category : $category->name",
-        'posts' => $category->posts->load('category', 'author')
-    ]);
-});
-
-Route::get('/posts/author/{author:username}', function (User $author) {
-    return view('posts', [
-        'title' => "Post by Author : $author->name",
-        'posts' => $author->posts->load('category', 'author')
     ]);
 });
